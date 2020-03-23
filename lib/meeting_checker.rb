b29edef1a7d235f3adcc522d9b239da816e9ff34
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-23 13:07:48 +0000

    Clean up code and refactor

--- lib/meeting_checker.rb
+++ lib/meeting_checker.rb
@@ -2,24 +2,25 @@ class MeetingChecker
 
   def availability(meetings)
     
-    sorted_meetings = meetings.sort! # Sort meetings so meetings that need to be merged are next to each other.
+    sorted_meetings = meetings.sort
 
-    merged_meetings = [sorted_meetings[0]] # Place the first meeting in the merged meetings array.
+    merged_meetings = [sorted_meetings[0]]
 
-    sorted_meetings[1..-1].each do |current_meeting_start, current_meeting_end| # Go through each meeting sequentially.
+    sorted_meetings[1..-1].each do |current_meeting_start, current_meeting_end|
 
-      last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1] # Pull out the last merged meetings start and end time.
+      last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
 
-      if current_meeting_start <= last_merged_meeting_end # If the current meeting's start is earlier than the last meeting's end.
+      if current_meeting_start <= last_merged_meeting_end 
  
-        merged_meetings[-1] = last_merged_meeting_start, [last_merged_meeting_end, current_meeting_end].max # Alter the last meeting's end time to the maximum end time.
+        merged_meetings[-1] = [last_merged_meeting_start, [last_merged_meeting_end, current_meeting_end].max]
 
       else
 
-        merged_meetings << [current_meeting_start, current_meeting_end] # If there's no overlap, add the current meeting to the merged meetings array.
+        merged_meetings << [current_meeting_start, current_meeting_end]
 
       end
     end
-    merged_meetings # return the merged meetings list
+    
+    merged_meetings
   end
 end
