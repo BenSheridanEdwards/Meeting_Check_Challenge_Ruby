4c7e1854ed9337f91114bc07ac7018c5ea8b2dd0
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-23 12:58:00 +0000

    TDD MeetingChecker availability function

--- lib/meeting_checker.rb
+++ lib/meeting_checker.rb
@@ -1,7 +1,25 @@
 class MeetingChecker
 
   def availability(meetings)
-    return meetings
-  end
+    
+    sorted_meetings = meetings.sort! # Sort meetings so meetings that need to be merged are next to each other
 
+    merged_meetings = [sorted_meetings[0]] # Place the first meeting in the merged meetings array
+
+    sorted_meetings[1..-1].each do |current_meeting_start, current_meeting_end| # Go through each meeting sequentially
+
+      last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1] # Pull out the last merged meetings start and end time
+
+      if current_meeting_start <= last_merged_meeting_end
+ 
+        merged_meetings[-1] = last_merged_meeting_start, [last_merged_meeting_end, current_meeting_end].max
+
+      else
+
+        merged_meetings << [current_meeting_start, current_meeting_end]
+
+      end
+    end
+    merged_meetings
+  end
 end
--- spec/meeting_checker_spec.rb
+++ spec/meeting_checker_spec.rb
@@ -2,8 +2,15 @@ require_relative './../lib/meeting_checker.rb'
 
 describe MeetingChecker do
 
-  it 'accepts an array of meetings and outputs that same array if there are no overlaps' do
+  let(:Checker) { MeetingChecker.new }
+  let(:meetings) { [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]] }
+
+  it 'accepts an array of meetings with no overlap and returns/outputs that same array' do
     expect(subject.availability([[1, 2], [4, 5], [6, 7]])).to eq [[1, 2], [4, 5], [6, 7]]
+  end
+
+  it 'accepts an array of meetings with one overlap and returns the two overlapped meetings as one meeting' do
+    expect(subject.availability([[0, 1], [3, 5], [4, 8], [10, 12]])).to eq [[0, 1], [3, 8], [10, 12]]
   end
 
 end
