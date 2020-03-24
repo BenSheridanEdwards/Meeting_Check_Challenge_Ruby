39efc66e09da957241e2432f1e1a8cbfbad40fa0
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-24 18:10:22 +0000

    Refactor tests for reversing in place challenge

--- spec/reversing_in_place_spec.rb
+++ spec/reversing_in_place_spec.rb
@@ -2,12 +2,25 @@ require 'reversing_in_place'
 
 describe InPlace do
 
-  it "accepts a string and reverses its order in place" do
-    expect(subject.reversing("12345")).to eq "54321"
-  end
+  describe '.reversing' do
 
-  it 'accepts an array and reverses its order in place' do
-    expect(subject.reversing(["1", "2", "3", "4", "5"])).to eq ["5", "4", "3", "2", "1"]
-  end
+    context "accepts string elements and reverses their order in-place" do
 
+      it "accepts '12345' and reverses its order to '54321'" do
+        expect(subject.reversing('12345')).to eq '54321'
+      end
+
+      it "accepts an array of strings ['1', '2', '3', '4', '5'] and reverses its order to ['5', '4', '3', '2', '1']" do
+        expect(subject.reversing(['1', '2', '3', '4', '5'])).to eq ['5', '4', '3', '2', '1']
+      end
+
+    end
+
+    context "accepts an array of numbers and reverses its order in-place" do 
+
+      it 'accepts [1, 2, 3, 4, 5] and returns [5, 4, 3, 2, 1]' do
+        expect(subject.reversing([1, 2, 3, 4, 5])).to eq [5, 4, 3, 2, 1]
+      end
+    end
+  end
 end
