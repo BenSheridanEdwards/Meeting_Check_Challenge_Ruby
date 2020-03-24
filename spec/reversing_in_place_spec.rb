cdd95adb43b6006961d0acbd34fa9fe11c726301
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-24 10:20:08 +0000

    Test arrays on InPlace reversing method

--- spec/reversing_in_place_spec.rb
+++ spec/reversing_in_place_spec.rb
@@ -5,5 +5,9 @@ describe InPlace do
   it "accepts a string and reverses its order in place" do
     expect(subject.reversing("12345")).to eq "54321"
   end
-  
+
+  it 'accepts an array and reverses its order in place' do
+    expect(subject.reversing(["1", "2", "3", "4", "5"])).to eq ["5", "4", "3", "2", "1"]
+  end
+
 end
