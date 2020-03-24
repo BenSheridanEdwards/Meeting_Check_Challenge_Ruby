7c71b573fb9f89e0811b0d7d09006c9b3c276012
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-24 10:19:34 +0000

    Create reversing method testing strings

--- lib/reversing_in_place.rb
+++ lib/reversing_in_place.rb
@@ -1,3 +1,15 @@
 class InPlace
 
+  def reversing(data)
+    left_index = 0
+    right_index = data.length - 1
+    while left_index < right_index
+      data[left_index], data[right_index] = \
+      data[right_index], data[left_index]
+      left_index += 1
+      right_index -= 1
+    end
+    data
+  end
+
 end
--- spec/reversing_in_place_spec.rb
+++ spec/reversing_in_place_spec.rb
@@ -2,4 +2,8 @@ require 'reversing_in_place'
 
 describe InPlace do
 
+  it "accepts a string and reverses its order in place" do
+    expect(subject.reversing("12345")).to eq "54321"
+  end
+  
 end
