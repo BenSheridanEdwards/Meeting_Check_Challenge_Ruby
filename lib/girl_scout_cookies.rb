6bb88d6bdcecde83e9cc50cf42001348720ccd41
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-26 11:50:24 +0000

    Create method merge_arrays with TDD concatinating and sorting the two arrays

--- lib/girl_scout_cookies.rb
+++ lib/girl_scout_cookies.rb
@@ -1,2 +1,6 @@
 class CookieOrders
+
+  def merge_arrays(my_array, alices_array)
+    return (my_array + alices_array).sort
+  end
 end
--- spec/girl_scout_cookies_spec.rb
+++ spec/girl_scout_cookies_spec.rb
@@ -1,4 +1,12 @@
 require 'girl_scout_cookies'
 
 describe CookieOrders do
+
+  describe '.merge_arrays' do
+
+    it 'merges [3, 4, 6, 10, 11, 15] & [1, 5, 8, 12, 14, 19] and outputs [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]' do
+      expect(subject.merge_arrays([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19])).to eq [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+    end 
+  end
+
 end
