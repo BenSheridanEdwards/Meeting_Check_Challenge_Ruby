06cc47c2f5da8875e5543674f68d00fb50e99260
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-26 21:16:19 +0000

    Write tests for evaluate true or false given the same movies but different flight lengths

--- spec/inflight_entertainment_spec.rb
+++ spec/inflight_entertainment_spec.rb
@@ -1,5 +1,17 @@
 require 'inflight_entertainment'
 
 describe InflightEntertainment do
+
+  context 'when there is two movies that can fill the flight time' do
+    it 'evaluates the movie lengths of [2, 4] for a 6 hour flight, and returns true' do
+      expect(subject.can_two_movies_fill_flight?([2, 4], 6)).to eq true
+    end
+  end
+
+  context 'when there is no two movies that can fill the flight time' do
+    it 'evaluates the movie lengths of [2, 4] for a 1 hour flight, and returns false' do
+      expect(subject.can_two_movies_fill_flight?([2, 4], 1)).to eq false
+    end
+  end
   
 end
