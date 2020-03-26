f481a300d3dec1dfee6ce9a86f4383a12ed9d42f
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-26 21:18:30 +0000

    Test for edge cases, only one movie or no movies given

--- spec/inflight_entertainment_spec.rb
+++ spec/inflight_entertainment_spec.rb
@@ -13,5 +13,15 @@ describe InflightEntertainment do
       expect(subject.can_two_movies_fill_flight?([2, 4], 1)).to eq false
     end
   end
+
+  context 'when there is only movie or no movies to fill the flight time' do
+    it 'evaluates the movie length of [6] for a 6 hour flight, and returns false' do
+      expect(subject.can_two_movies_fill_flight?([2, 4], 1)).to eq false
+    end
+
+    it 'evaluates no movies [] for a 2 hour flight, and returns false' do
+      expect(subject.can_two_movies_fill_flight?([2, 4], 1)).to eq false
+    end
+  end
   
 end
