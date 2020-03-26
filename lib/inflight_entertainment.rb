109f7be84c75ecaff5d3887b07a52983d6550d53
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-26 21:11:57 +0000

    Submit working solution using a set

--- lib/inflight_entertainment.rb
+++ lib/inflight_entertainment.rb
@@ -1,2 +1,14 @@
+require 'set'
+
 class InflightEntertainment
+
+  def can_two_movies_fill_flight?(movie_lengths, flight_length)
+    movie_lengths_seen = Set.new
+
+    movie_lengths.each do |movie_time|
+      return true if movie_lengths_seen.include?(flight_length - movie_time)
+      movie_lengths_seen.add(movie_time)
+    end
+    return false
+  end
 end
