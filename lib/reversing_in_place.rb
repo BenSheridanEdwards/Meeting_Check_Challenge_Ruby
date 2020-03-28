a054aa74963cd726badeebe6077daf6dd78c2c2a
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-28 16:51:33 +0000

    Write helper method reverse_characters

--- lib/reversing_in_place.rb
+++ lib/reversing_in_place.rb
@@ -13,7 +13,20 @@ class InPlace
   end
 
   def reverse_words!(message)
-    return 'steal pound cake'
+    
   end
 
+
+  # Helper method for reverse_words!
+
+  def reverse_characters!(message, left_index, right_index)
+
+    while left_index < right_index
+      message[left_index], message[right_index] =
+        message[right_index], message[left_index]
+
+      left_index += 1
+      right_index += 1
+    end
+  end
 end
