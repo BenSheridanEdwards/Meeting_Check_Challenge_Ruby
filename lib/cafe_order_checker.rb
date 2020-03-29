d9fa0004a7d2ccb43e53a6385791c8adbf553b8d
bensheridanedwards <bensheridanedwards@gmail.com>
2020-03-29 20:23:19 +0100

    Write code and tests for cafe order checker challenge

--- lib/cafe_order_checker.rb
+++ lib/cafe_order_checker.rb
@@ -1,2 +1,33 @@
 class CafeOrderChecker
+
+  def first_come_first_served?(take_out_orders, dine_in_orders, served_orders)
+
+    take_out_index = 0
+    dine_in_index = 0
+    take_out_max_index = take_out_orders.length - 1
+    dine_in_max_index = dine_in_orders.length - 1
+    
+    
+    served_orders.each { |order|
+      
+      if take_out_index <= take_out_max_index && order == take_out_orders[take_out_index]
+        
+        take_out_index += 1
+        
+      elsif dine_in_index <= dine_in_max_index && order == dine_in_orders[dine_in_index]
+        
+        dine_in_index += 1
+        
+      else
+        return false
+      end
+    
+    }
+    
+    if dine_in_index != dine_in_orders.length || take_out_index != take_out_orders.length 
+      return false
+    end
+    
+    true
+  end
 end
--- spec/cafe_order_checker_spec.rb
+++ spec/cafe_order_checker_spec.rb
@@ -1,5 +1,26 @@
 require 'cafe_order_checker'
 
 describe CafeOrderChecker do
-  
+
+  describe '.first_come_first_served?' do
+    context 'both registers have the same number of orders' do
+      it 'accepts [1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6] and returns true' do
+        expect(subject.first_come_first_served?([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])).to eq true
+      end
+
+      it 'accepts [1, 5, 4], [2, 3, 6], [1, 2, 3, 4, 5, 6] and returns false' do
+        expect(subject.first_come_first_served?([1, 5, 4], [2, 3, 6], [1, 2, 3, 4, 5, 6])).to eq false
+      end
+    end
+
+    context 'registers have different lengths' do
+      it 'accepts [1, 5], [2, 3, 6], [1, 2, 3, 5, 6], and returns true' do
+        expect(subject.first_come_first_served?([1, 5], [2, 3, 6], [1, 2, 3, 5, 6])).to eq true
+      end
+
+      it 'accepts [1, 5], [2, 3, 6], [1, 2, 6, 3, 5] and returns false' do
+        expect(subject.first_come_first_served?([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])).to eq false
+      end
+    end
+  end
 end
